481e5e8777f6d283d744a3ea2236d7a9564b51af
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-12-06 08:21:58 +0100

    add volumes

---
+++ db_showpage/mysql_docs/initial/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE testdbtz4 CHARACTER SET utf8;
+
+CREATE TABLE IF NOT EXISTS
+`testdbtz4`.`adaspace2`
+(`DBID` varchar(4), `ASSOgesamt` varchar(10), `ASSOfrei` varchar(8), `DATAgesamt` varchar(10), `DATAfrei` varchar(8)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;;
+/*
+LOAD DATA LOCAL INFILE '/home/ADASPACE.csv'
+INTO TABLE `testdbtz4`.`adaspace2`
+FIELDS TERMINATED BY ','
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+*/
+
